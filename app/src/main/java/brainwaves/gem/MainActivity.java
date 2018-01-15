e5c9ffc4dd500abbbd5e7caeb9d40566d827a390
Ahmed Abdelfattah <ahmed.m.abdelftah@gmail.com>
2018-01-15 11:49:59 +0200

    init project

---
+++ app/src/main/java/brainwaves/gem/MainActivity.java
@@ -0,0 +1,202 @@
+package brainwaves.gem;
+
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.design.widget.NavigationView;
+import android.support.v13.app.FragmentPagerAdapter;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import brainwaves.gem.fragments.Page1Fragment;
+import brainwaves.gem.fragments.Page2Fragment;
+import brainwaves.gem.fragments.Page3Fragment;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    private final String LOG_TAG = MainActivity.class.getSimpleName();
+
+    // Titles of the individual pages (displayed in tabs)
+    private final String[] PAGE_TITLES = new String[] {
+            "Visit",
+            "EXHIBITION",
+            "HIGHLIGHTS",
+            "TODAY'S EVENT",
+            "STAFF PICKS",
+            "FOR MEMBERS",
+            "FEATURED EVENTS",
+            "THE LATEST"
+    };
+
+    // The fragments that are used as the individual pages
+    private final Fragment[] PAGES = new Fragment[] {
+            new Page1Fragment(),
+            new Page2Fragment(),
+            new Page3Fragment(),
+            new Page3Fragment(),
+            new Page3Fragment(),
+            new Page3Fragment(),
+            new Page3Fragment(),
+            new Page3Fragment()
+    };
+
+    // The ViewPager is responsible for sliding pages (fragments) in and out upon user input
+    private ViewPager mViewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //toolbar.setLogo(R.drawable.gem);
+        setSupportActionBar(toolbar);
+
+
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        mViewPager = (ViewPager) findViewById(R.id.viewpager);
+        mViewPager.setAdapter(new MyPagerAdapter(getFragmentManager()));
+
+        // Connect the tabs with the ViewPager (the setupWithViewPager method does this for us in
+        // both directions, i.e. when a new tab is selected, the ViewPager switches to this page,
+        // and when the ViewPager switches to a new page, the corresponding tab is selected)
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        tabLayout.setTabMode(TabLayout.MODE_SCROLLABLE);
+        tabLayout.setupWithViewPager(mViewPager);
+
+        final AppBarLayout appBarLayout=(AppBarLayout)findViewById(R.id.appbar);
+        tabLayout.setOnTabSelectedListener(
+                new TabLayout.ViewPagerOnTabSelectedListener(mViewPager) {
+
+                    @Override
+                    public void onTabSelected(TabLayout.Tab tab) {
+                        super.onTabSelected(tab);
+                     //   Toast.makeText(MainActivity.this,PAGE_TITLES[tab.getPosition()], Toast.LENGTH_SHORT).show();
+                        int tabIndex=tab.getPosition();
+                        switch (tabIndex){
+                            case 0:
+                                appBarLayout.setBackgroundColor(Color.parseColor("#FF1E1E"));
+                                break;
+                            case 1:
+                                appBarLayout.setBackgroundColor(Color.parseColor("#6E0000"));
+                                break;
+                            case 2:
+                                appBarLayout.setBackgroundColor(Color.parseColor("#DC0051"));
+                                break;
+                            case 3:
+                                appBarLayout.setBackgroundColor(Color.parseColor("#7A0031"));
+                                break;
+                            case 4:
+                                appBarLayout.setBackgroundColor(Color.parseColor("#670050"));
+                                break;
+                            case 5:
+                                appBarLayout.setBackgroundColor(Color.parseColor("#290348"));
+                                break;
+                            case 6:
+                                appBarLayout.setBackgroundColor(Color.parseColor("#476ACB"));
+                                break;
+                            case 7:
+                                appBarLayout.setBackgroundColor(Color.parseColor("#017F72"));
+                                break;
+                        }
+                    }
+                }
+        );
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    /* PagerAdapter for supplying the ViewPager with the pages (fragments) to display. */
+    public class MyPagerAdapter extends FragmentPagerAdapter {
+
+        public MyPagerAdapter(FragmentManager fragmentManager) {
+            super(fragmentManager);
+        }
+        @Override
+        public Fragment getItem(int position) {
+            return PAGES[position];
+        }
+
+        @Override
+        public int getCount() {
+            return PAGES.length;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return PAGE_TITLES[position];
+        }
+
+    }
+}
---
+++ app/src/main/java/brainwaves/gem/fragments/Page3Fragment.java
@@ -0,0 +1,19 @@
+package brainwaves.gem.fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import brainwaves.gem.R;
+
+/* Fragment used as page 3 */
+public class Page3Fragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_page3, container, false);
+        return rootView;
+    }
+}
