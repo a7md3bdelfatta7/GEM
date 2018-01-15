caa0ca73d9658177326db9b23c46b22d490fb5ab
Ahmed Abdelfattah <ahmed.m.abdelftah@gmail.com>
2018-01-15 23:36:25 +0200

    user type activity

--- app/src/main/java/brainwaves/gem/ChooseLangActivity.java
+++ app/src/main/java/brainwaves/gem/ChooseLangActivity.java
@@ -36,7 +36,7 @@ public class ChooseLangActivity extends AppCompatActivity implements AdapterView
         nextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent=new Intent(ChooseLangActivity.this,MainActivity.class);
+                Intent intent=new Intent(ChooseLangActivity.this,ChooseUserType.class);
                 startActivity(intent);
             }
         });
