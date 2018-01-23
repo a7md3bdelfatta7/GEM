a5e70a87c5ca2673408ed0a8bd8f27dbaadb0602
HossamAhmed93 <hossamgamalabdelhaleem@gmail.com>
2018-01-23 15:56:26 +0200

    adding shipping activity

--- app/src/main/java/brainwaves/gem/HelperMenu/CollectionsActivity.java
+++ app/src/main/java/brainwaves/gem/HelperMenu/CollectionsActivity.java
@@ -1,9 +1,19 @@
 package brainwaves.gem.HelperMenu;
 
+import android.content.Intent;
+import android.os.Debug;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import java.util.ArrayList;
+
+import brainwaves.gem.MainActivity;
 import brainwaves.gem.R;
 
 public class CollectionsActivity extends AppCompatActivity {
@@ -14,6 +24,105 @@ public class CollectionsActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_collections);
+        LinearLayout collectionResultLayout = (LinearLayout) findViewById(R.id.collection_SearchResultLV);
+        collectionResultLayout.setVisibility(View.GONE);
+        LinearLayout collectionFirstLayout = (LinearLayout) findViewById(R.id.collection_FirstLayout);
+        collectionFirstLayout.setVisibility(View.VISIBLE);
+        LinearLayout collectionSecondLayout = (LinearLayout) findViewById(R.id.collection_FirstLayout);
+        collectionSecondLayout.setVisibility(View.VISIBLE);
+
+
+    }
+    public void searchOnClick(View v) {
+        TextView searchTxtView = (TextView) findViewById(R.id.collection_SearchTextView);
+        String searchData = searchTxtView.getText().toString();
+        LinearLayout collectionFirstLayout = (LinearLayout) findViewById(R.id.collection_FirstLayout);
+        LinearLayout collectionSecondLayout = (LinearLayout) findViewById(R.id.collection_SecondLayout);
+        LinearLayout collectionResultLayout = (LinearLayout) findViewById(R.id.collection_SearchResultLV);
+        if(collectionResultLayout.getChildAt(0).getId() == R.id.artifact_1){
+            ImageButton searchResultImgBtnasdas = (ImageButton) findViewById(collectionResultLayout.getChildAt(0).getId());
+        }
+        ImageButton searchResultImgBtn = (ImageButton) findViewById(collectionResultLayout.getChildAt(0).getId());
+
+        ArrayList<String> artifacts = new ArrayList<String>();
+        artifacts.add("tutAnkAmum");
+        artifacts.add("ramsis");
+        artifacts.add("shit");
+        artifacts.add("fuck");
+        artifacts.add("Lol");
+
+
+
+        int artifactsIndex = 0;
+        for(int i=0;i<artifacts.size();i++){
+            if (artifacts.get(i).contains(searchData)) {
+                artifactsIndex = i+1;
+                break;
+            }
+        }
+        collectionFirstLayout.setVisibility(View.GONE);
+        collectionSecondLayout.setVisibility(View.GONE);
+        collectionResultLayout.setVisibility(View.VISIBLE);
+
+        switch (artifactsIndex){
+            case 1:
+
+                //searchResultImgBtn.setImageDrawable(getResources().getDrawable(R.drawable.highlight_i));
+                searchResultImgBtn.setImageResource(R.drawable.highlight_i);
+                searchResultImgBtn.setId(R.id.artifact_1);
+                break;
+            case 2:
+                searchResultImgBtn.setImageResource(R.drawable.highlight_ii);
+                searchResultImgBtn.setId(R.id.artifact_2);
+                break;
+            case 3:
+                searchResultImgBtn.setImageResource(R.drawable.highlight_iii);
+                searchResultImgBtn.setId(R.id.artifact_3);
+                break;
+            case 4:
+                searchResultImgBtn.setImageResource(R.drawable.highlight_iv);
+                searchResultImgBtn.setId(R.id.artifact_4);
+                break;
+            case 5:
+                searchResultImgBtn.setImageResource(R.drawable.highlight_v);
+                searchResultImgBtn.setId(R.id.artifact_5);
+                break;
+            case 6:
+                searchResultImgBtn.setImageResource(R.drawable.highlight_xv);
+                searchResultImgBtn.setId(R.id.artifact_6);
+                break;
+            case 7:
+                searchResultImgBtn.setImageResource(R.drawable.highlight__i);
+                searchResultImgBtn.setId(R.id.artifact_7);
+                break;
+            case 8:
+                searchResultImgBtn.setImageResource(R.drawable.highlight__ii);
+                searchResultImgBtn.setId(R.id.artifact_8);
+                break;
+            case 9:
+                searchResultImgBtn.setImageResource(R.drawable.highlight__iii);
+                searchResultImgBtn.setId(R.id.artifact_9);
+                break;
+            case 10:
+                searchResultImgBtn.setImageResource(R.drawable.highlight__iv);
+                searchResultImgBtn.setId(R.id.artifact_10);
+                break;
+            case 11:
+                searchResultImgBtn.setImageResource(R.drawable.highlight__v);
+                searchResultImgBtn.setId(R.id.artifact_11);
+                break;
+            case 12:
+                searchResultImgBtn.setImageResource(R.drawable.highlight__xv);
+                searchResultImgBtn.setId(R.id.artifact_12);
+                break;
+        }
+
+    }
+    public void artifactsDetailsonClick(View v) {
+        Intent intent = new Intent(CollectionsActivity.this,
+                ArtifactsActivity.class);
+        intent.putExtra("id",""+v.getId());
+        startActivity(intent);
     }
     @Override
     protected  void onDestroy() {
--- app/src/main/java/brainwaves/gem/HelperMenu/MembershipActivity.java
+++ app/src/main/java/brainwaves/gem/HelperMenu/MembershipActivity.java
@@ -8,13 +8,23 @@ import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.ExpandableListView;
+import android.widget.RadioButton;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import brainwaves.gem.HelperClasses.ExpandableListAdapter;
 import brainwaves.gem.R;
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 public class MembershipActivity extends AppCompatActivity {
-
+    ExpandableListAdapter listAdapter;
+    ExpandableListView expListView;
+    List<String> listDataHeader;
+    HashMap<String, List<String>> listDataChild;
     @Override
     protected void attachBaseContext(Context newBase) {
         super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
@@ -26,28 +36,93 @@ public class MembershipActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_membership);
-        final CheckBox checked = (CheckBox) findViewById(R.id.membership_checkbox_i);
-        final CheckBox checked_ii = (CheckBox) findViewById(R.id.membership_checkbox_ii);
-        final CheckBox checked_iii = (CheckBox) findViewById(R.id.membership_checkbox_iii);
+
         Button submitButton = (Button) findViewById(R.id.membership_submit);
+
+
+
+        // get the listview
+        expListView = (ExpandableListView) findViewById(R.id.lvExp);
+        // preparing list data
+        prepareListData();
+
+        listAdapter = new ExpandableListAdapter(this, listDataHeader, listDataChild);
+
+        // setting list adapter
+        expListView.setAdapter(listAdapter);
+
+        expListView.expandGroup(1);
+        expListView.expandGroup(2);
+
+
         submitButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                if(checked.isChecked()){
-                    Toast.makeText(getApplicationContext(),"Thanks for submitting 3 Months",Toast.LENGTH_LONG).show();
-                }else if(checked_ii.isChecked()){
-                    Toast.makeText(getApplicationContext(),"Thanks for submitting 6 Months",Toast.LENGTH_LONG).show();
-                }else if(checked_iii.isChecked()){
-                    Toast.makeText(getApplicationContext(),"Thanks for submitting 1 Year",Toast.LENGTH_LONG).show();
-                }
+                
                 // Code here executes on main thread after user presses button
             }
         });
 
+
+    }
+    public void onRadioButtonClicked(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.membership_three_months:
+                if (checked)
+                    Toast.makeText(getApplicationContext(),"Thanks for submitting 3 Months",Toast.LENGTH_LONG).show();
+                    break;
+            case R.id.membership_six_months:
+                if (checked)
+                    Toast.makeText(getApplicationContext(),"Thanks for submitting 6 Months",Toast.LENGTH_LONG).show();
+                    break;
+            case R.id.membership_one_year:
+                if (checked)
+                    Toast.makeText(getApplicationContext(),"Thanks for submitting 1 Year",Toast.LENGTH_LONG).show();
+                    break;
+        }
     }
     @Override
     protected  void onDestroy() {
         super.onDestroy();
         Runtime.getRuntime().gc();
         System.gc();
+    }
+    private void prepareListData() {
+        listDataHeader = new ArrayList<String>();
+        listDataChild = new HashMap<String, List<String>>();
+
+        // Adding child data
+        listDataHeader.add("Children membership");
+        listDataHeader.add("Egyptian membership");
+        listDataHeader.add("Foreign membership");
+
+        // Adding child data
+        List<String> level0 = new ArrayList<String>();
+        level0.add("Free express admission for you and children under 18.");
+        level0.add("Free admission for your guest(s) on every visit.");
+
+
+        List<String> level1 = new ArrayList<String>();
+        level1.add("Early access to exhibitions and special viewing hours.");
+        level1.add("Invitation to buy ticket to members only events and parties.");
+
+
+        List<String> level2 = new ArrayList<String>();
+        level2.add("10% off at the Dining room at GEM.");
+        level2.add("10% off at GEM store.");
+
+
+
+
+
+        listDataChild.put(listDataHeader.get(0), level0); // Header, Child data
+        listDataChild.put(listDataHeader.get(1), level1);
+        listDataChild.put(listDataHeader.get(2), level2);
+
+
+
     }
 }
