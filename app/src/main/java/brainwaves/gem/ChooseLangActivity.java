da1d7497a3d6e7bca33c20543e8d022cc71cb206
HossamAhmed93 <hossamgamalabdelhaleem@gmail.com>
2018-01-29 16:32:44 +0200

    startup video

--- app/src/main/java/brainwaves/gem/ChooseLangActivity.java
+++ app/src/main/java/brainwaves/gem/ChooseLangActivity.java
@@ -3,6 +3,8 @@ package brainwaves.gem;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Typeface;
+import android.net.Uri;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,7 +12,10 @@ import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.MediaController;
 import android.widget.Spinner;
+import android.widget.VideoView;
 
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
@@ -31,6 +36,9 @@ public class ChooseLangActivity extends AppCompatActivity implements AdapterView
         setContentView(R.layout.choose_lang);
         Typeface custom_font = Typeface.createFromAsset(getAssets(),  "fonts/gothicb0_bold.ttf");
 
+        final VideoView videoView = (VideoView)findViewById(R.id.mainSponsor);
+        final LinearLayout chooseLang = (LinearLayout) findViewById(R.id.activity_login);
+        final LinearLayout chooseLangAd = (LinearLayout) findViewById(R.id.chooseLangAd);
         Spinner spinner = (Spinner) findViewById(R.id.lang_spinner);
 
         // Create an ArrayAdapter using the string array and a default spinner layout
@@ -52,6 +60,20 @@ public class ChooseLangActivity extends AppCompatActivity implements AdapterView
                 startActivity(intent);
             }
         });
+        videoView.setVideoURI(Uri.parse("android.resource://" + getPackageName() +"/"+R.raw.startupad));
+        videoView.setMediaController(new MediaController(this));
+        videoView.requestFocus();
+        videoView.start();
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                chooseLangAd.setVisibility(View.GONE);
+                chooseLang.setVisibility(View.VISIBLE);
+                //Do something after 100ms
+            }
+        }, 5000);
 
     }
 
